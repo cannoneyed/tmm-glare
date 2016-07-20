267a11220d0f4666a5f95b9ec52b8d71ff6c303d
Andy Coenen <andrew.coenen@gmail.com>
2016-07-19 23:25:39 -0700

    Stop audio on signout

--- src/core/auth/async/signOut.js
+++ src/core/auth/async/signOut.js
@@ -5,7 +5,13 @@ import {
 // Manage signing out of the application
 export default function signOutAsync() {
   return (dispatch, getState) => {
-    const { firebase } = getState()
+    const { firebase, listen } = getState()
+    const { soundCloudAudio } = listen
+
+    if (soundCloudAudio) {
+      soundCloudAudio.stop()
+    }
+
     firebase.auth().signOut().then(() => {
       dispatch(signOutSuccess())
     })
