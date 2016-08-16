7ccfc4f6666e815af560108479ddca37f09ddd0c
Andy Coenen <andrew.coenen@gmail.com>
2016-08-15 20:56:54 -0700

    Cluster the server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const throng = require('throng')
+const WORKERS = process.env.WEB_CONCURRENCY || 1
 require('dotenv').config()
 
 const express = require('express')
@@ -31,13 +33,21 @@ app.use(router)
 //=========================================================
 //  START SERVER
 //---------------------------------------------------------
-
-initializeApp().then(() => {
-  app.listen(app.get('port'), app.get('host'), error => {
-    if (error) {
-      logger.error(error)
-    } else {
-      logger.info(`Server listening @ ${app.get('host')}:${app.get('port')}`)
-    }
+const start = (id) => {
+  initializeApp().then(() => {
+    app.listen(app.get('port'), app.get('host'), error => {
+      if (error) {
+        logger.error(error)
+      } else {
+        logger.info(`Server ${id} listening @ ${app.get('host')}:${app.get('port')}`)
+      }
+    })
   })
+}
+
+// Cluster the server
+throng({
+  start,
+  workers: WORKERS,
+  lifetime: Infinity
 })
