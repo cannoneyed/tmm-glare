e28bbe74b8160b8ea639d5d7b369e7dffc3e90ce
Andy Coenen <andrew.coenen@gmail.com>
2016-09-12 09:43:39 -0700

    Add .env stuff

--- server/firebase/index.js
+++ server/firebase/index.js
@@ -3,6 +3,8 @@ const GeoFire = require('geofire')
 
 const { firebaseConfig } = require('config')
 
+console.log(firebaseConfig)
+
 const firebase = Firebase.initializeApp(firebaseConfig)
 const geofire = {
   beaconLocations: new GeoFire(firebase.database().ref().child('beaconLocations')),
