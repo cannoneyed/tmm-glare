85907c6d06b85e361b2ca27dbb55e42a7e711a7d
Andy Coenen <andrew.coenen@gmail.com>
2016-09-12 00:03:57 -0700

    Update server

--- server/server.js
+++ server/server.js
@@ -4,11 +4,8 @@ require('dotenv').config()
 
 const express = require('express')
 const logger = require('winston')
-// const { firebase } = require('./firebase')
-// const util = require('./util')
 const initializeApp = require('./initialize')
-// const processUserGraph = require('./actions/process-user-graph')
-// const webUtil = require('./webUtil')
+const webUtil = require('./webUtil')
 
 //=========================================================
 //  SETUP
@@ -22,13 +19,13 @@ app.set('port', process.env.PORT || 3001)
 //=========================================================
 //  ROUTER
 //---------------------------------------------------------
-// const router = new express.Router()
-//
-// router.get('/graph/:id', webUtil.handle(function* gen(req) {
-//   return processUserGraph({ data: { userId: req.params.id }})
-// }))
-//
-// app.use(router)
+const router = new express.Router()
+
+router.get('/hello', webUtil.handle(function* gen() {
+  return 'dog!'
+}))
+
+app.use(router)
 
 //=========================================================
 //  START SERVER
