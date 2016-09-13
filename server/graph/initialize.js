f7ebf90b34e1b9d77a6e24da0b74975d573446a2
Andy Coenen <andrew.coenen@gmail.com>
2016-09-12 20:23:42 -0700

    Add logging

--- server/graph/initialize.js
+++ server/graph/initialize.js
@@ -9,7 +9,13 @@ const db = firebase.database().ref()
 
 module.exports = P.coroutine(function* () {
   const timeStart = Date.now()
+
+  console.log('HEY!')
+
   const snapshot = yield db.child('connections').once('value')
+
+  console.log('HO!')
+
   const connections = util.recordsFromSnapshot(snapshot)
 
   _.each(connections, connection => {
