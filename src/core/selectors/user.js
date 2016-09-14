31c90542e38fa0d73969d825ffa6e1c0c8ca5959
Andy Coenen <andrew.coenen@gmail.com>
2016-09-13 21:32:41 -0700

    Add admin selector privelege

--- src/core/selectors/user.js
+++ src/core/selectors/user.js
@@ -1,9 +1,14 @@
 import { appConstants } from 'src/constants'
 
 export function getRemainingGives(state) {
-  const { user: { connections } } = state
+  const { user: { connections, isAdmin } } = state
 
   const remaining = appConstants.maximumGives - Object.keys(connections || {}).length
+
+  if (isAdmin) {
+    return 999
+  }
+
   return Math.max(0, remaining) // No negative numbers
 }
 
