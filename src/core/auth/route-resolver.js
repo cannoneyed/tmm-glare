5dc61e41bb52920ba71d4d4bc0c25bae8af19b2e
Andy Coenen <andrew.coenen@gmail.com>
2016-09-18 10:31:13 -0700

    Just a few routing fixes

--- src/core/auth/route-resolver.js
+++ src/core/auth/route-resolver.js
@@ -1,24 +1,26 @@
 export default function authRouteResolver(getState) {
   return (nextState, replace) => {
     const { auth, user } = getState()
-    const { pathname } = nextState.location
+    let { pathname } = nextState.location
+    if (pathname === '/') {
+      pathname = '/connect'
+    }
 
     const hasAccess = !!user && user.hasAccess
     const authenticated = auth.authenticated
-    let target = `/${pathname}`
 
-    if (!authenticated && target !== '/sign-in') {
+    if (!authenticated && pathname !== 'sign-in') {
       return replace({ pathname: '/sign-in' })
     }
 
-    if (target === '/sign-in') {
+    if (pathname === 'sign-in') {
       return
     }
 
     if (authenticated && !hasAccess) {
-      if (target === '/intro') {
+      if (pathname === 'intro') {
         return replace({ pathname: '/intro' })
-      } else if (target !== '/connect') {
+      } else if (pathname !== 'connect') {
         replace({ pathname: '/connect' })
       }
     }
