1ca1e46cb55ad036e7259c7d02cfdb8888479164
Andy Coenen <andrew.coenen@gmail.com>
2016-10-11 15:16:25 -0400

    Process user graph from server

--- server/actions/process-user-graph/index.js
+++ server/actions/process-user-graph/index.js
@@ -5,19 +5,29 @@ const graphData = require('../../graph/data')
 const { firebase } = require('../../firebase')
 const helpers = require('./helpers')
 
-module.exports = function processUserStats({ data, resolve, reject }) {
+module.exports = function processUserGraph({ data, resolve, reject }) {
   const { userId } = data
 
-  return P.try(() => {
+  return P.resolve().then(() => {
     const g = graphData.getGraph()
 
     const connected = g.successors(userId) || []
     const own = g.node(userId)
 
     if (!own) {
-      return resolve()
+      return
     }
 
+    // Process the graph data
+    // Formatted as { from: [to, to] }
+    const connections = {}
+    connected.forEach(userId => {
+      const node = g.node(userId)
+      const { from } = node
+      connections[from] = (connections[from] || []).concat(userId)
+    })
+
+    // Process the graph stats
     const total = connected.length
 
     let maxDistance = 0
@@ -28,13 +38,21 @@ module.exports = function processUserStats({ data, resolve, reject }) {
       maxDistance = distance > maxDistance ? distance : maxDistance
     })
 
-    return {
-      total,
-      maxDistance,
+    const score = Math.floor(maxDistance) + (total * 5)
+
+    const processed = {
+      data: {
+        connections,
+      },
+      stats: {
+        total,
+        maxDistance,
+        score,
+      },
     }
-  })
-  .then(processed => {
-    return firebase.database().ref('userStats').child(userId).set(processed)
+
+    logger.info(`Processed graph for user ${userId}`)
+    return firebase.database().ref('userGraph').child(userId).set(processed)
   })
   .then(resolve)
   .catch(err => {
