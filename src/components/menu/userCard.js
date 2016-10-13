3603a1d3e9481453e63bc97842ff124a2873dd5a
Andy Coenen <andrew.coenen@gmail.com>
2016-10-13 13:35:04 -0400

    Fix user access bug and bump to 1.0

--- src/components/menu/userCard.js
+++ src/components/menu/userCard.js
@@ -7,6 +7,7 @@ import UserAvatar from 'src/components/shared/userAvatar'
 
 const UserCard = (props) => {
   const {
+    hasAccess,
     remaining,
     score,
     user
@@ -17,27 +18,37 @@ const UserCard = (props) => {
     paddingRight: 15,
   }
 
+  const influence = (
+    <div className="user-stats">
+      Influence: <span className="user-influence">{score}</span>
+    </div>
+  )
+
+  const gives = (
+    <div className="user-stats">
+      Gives Remaining: <span className="user-gives">{remaining}</span>
+    </div>
+  )
+
   return (
     <div className="user-card">
       <UserAvatar profileImageURL={user.profileImageURL} style={avatarStyle} />
       <div className="user-name">{user.displayName}</div>
-      <div className="user-stats">
-        Influence: <span className="user-influence">{score}</span>
-      </div>
-      <div className="user-stats">
-        Gives Remaining: <span className="user-gives">{remaining}</span>
-      </div>
+      { hasAccess ? influence : null }
+      { hasAccess ? gives : null }
     </div>
   )
 }
 
 UserCard.propTypes = {
+  hasAccess: PropTypes.bool,
   remaining: PropTypes.number,
   score: PropTypes.number,
   user: PropTypes.object,
 }
 
 export default connect(state => ({
+  hasAccess: state.user && state.user.hasAccess,
   remaining: getRemainingGives(state),
   score: getConnectionsScore(state),
   user: state.user,
