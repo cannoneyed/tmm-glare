d4dc3ac7859e14fdc296619e36c5baebd6a86125
Andy Coenen <andrew.coenen@gmail.com>
2016-12-04 16:53:19 -0800

    Begin email service

--- config/default.js
+++ config/default.js
@@ -2,4 +2,7 @@ module.exports = {
   geonames: {
     username: process.env.GEONAMES_USERNAME,
   },
+  postmark: {
+    id: process.env.POSTMARK_ID,
+  }
 }
---
+++ scripts/05-test-email.js
@@ -0,0 +1,20 @@
+import ReactHTMLEmail from 'react-html-email'
+ReactHTMLEmail.injectReactEmailAttributes()
+
+require('dotenv').config()
+const config = require('config')
+import postmark from 'postmark'
+
+import email from '../email/welcome'
+const html = ReactHTMLEmail.renderEmail(email)
+
+const client = new postmark.Client(config.postmark.id)
+
+client.sendEmail({
+  From: 'm@glare.fm',
+  To: 'andrew.coenen@gmail.com',
+  Subject: 'Test',
+  HtmlBody: html,
+}, (err, result) => {
+  console.log('🍕', err, result)
+})
