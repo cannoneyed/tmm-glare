5a87b8a1b246181ca2cca843971bd4ee81aa592e
Andy Coenen <andrew.coenen@gmail.com>
2016-12-05 14:52:09 -0800

    Update faqs

---
+++ src/components/help/faqs.js
@@ -0,0 +1,25 @@
+import React from 'React'
+
+export const faqs = [{
+  title: 'How does glare.fm work?',
+  answer: [
+    'Glare.fm is the prerelease app for The M Machine\'s upcoming Glare album. Glare.fm is an experiment all about giving music to someone else, person-to-person, and watching as the music spreads across the world. Here\'s how it works:',
+    'You can login at glare.fm anytime, but to listen to music, you\'ll have to get access from someone who’s already unlocked it.',
+    'In order to unlock glare.fm, someone who already has access must share it with you IN PERSON.',
+    'Once activated you\'ll have four songs to listen to, and you can unlock two more each time you share it with someone else.',
+    'You\'ll also be able to follow the album as it travels across the globe, and track your personal influence on its spread.',
+  ]
+}, {
+  title: 'Why did you make glare.fm?',
+  answer: [
+    'When we finished writing our new album, Glare, we knew we wanted to try something special for the release — and we kept coming back to the idea that we wanted our listeners to share the album with each other in person, over a conversation. As we started to develop the mechanics of glare.fm, it became clear that there was a magic in knowing that the music began its life with us, and made its way — one human at a time — to you.'
+  ]
+}, {
+  title: 'Why can\'t I give or receive the album?',
+  answer: [
+    'Glare.fm uses geolocation to make giving and receiving the album in-person possible. In order to use glare.fm, you must enable geolocation in your browser. There are a number of different ways to turn geolocation on and off, depending on your browser.',
+    <a key="chrome" href="https://support.google.com/chrome/answer/142065?hl=en" target="_new">Directions for Chrome</a>,
+    <a key="safari" href="https://support.apple.com/en-us/HT203033" target="_new">Directions for Safari</a>,
+    'Once geolocation is enabled, be sure to allow geolocation when prompted by your browser, and make sure that you\'re next to the person you\'re trying to give to / receive from',
+  ]
+}]
